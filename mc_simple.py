c32fb8f817bae20bad323f43521cf214adc1ef94
Neill Warrington <tomu@Neills-MacBook-Pro.local>
2022-11-10 15:21:59 -0800

    Added functions to mc_simple.py

--- mc_simple.py
+++ mc_simple.py
@@ -3,25 +3,18 @@ import numpy as np
 import multiprocessing as mp
 import numba as nb
 import time
+import copy
 
 
+#still need to debug this
 def Action(T,F,beta,coupling,s):
 
     Nx, Nt = T.shape
     dt = beta/Nt
 
-    CT = np.cos(T)    
-    ST = np.sin(T)    
-    CF = np.cos(F)    
-    SF = np.sin(F)
-
-    Ox = ST*CF
-    Oy = ST*SF
-    Oz = CT
-
-    tot = 0.0
-    vol = 1.0
-    bp  = 1.0
+    CT, ST, CF, SF = np.cos(T), np.sin(T), np.cos(F), np.sin(F)
+    Ox, Oy, Oz = ST*CF, ST*SF, CT
+    tot, vol, bp = 0.0, 1.0, 1.0
 
     for x in range(Nx):
         for t in range(Nt):
@@ -37,24 +30,93 @@ def Action(T,F,beta,coupling,s):
 
 
 
-def Perturb(T,F):
+def Perturb(T,F,dth,dphi):
     
     Nx, Nt = T.shape
-    Ap = copy.deepcopy(A)
+    Tp = copy.deepcopy(T)
+    Fp = copy.deepcopy(F)
 
+    delta_T = np.random.normal(scale = dth, size  = T.shape)
+    delta_F = np.random.normal(scale = dphi, size = F.shape)
+
+    Tp += delta_T
+    Fp += delta_F
+
+    #this step just ensures we stay in the proper integration domain
     for t in range(Nt):
         for x in range(Nx):
  
-            if np.random.random() < frac:
-                flip = np.random.choice([-1,0,1])
-                tmp = Ap[t,x] + flip
-                if tmp > S:
-                    Ap[t,x] = -S 
-                elif tmp < -S:
-                    Ap[t,x] = S
-                else:
-                    Ap[t,x] = tmp
-    return Ap
+            if Tp[x,t] > np.pi:
+                eps = Tp[x,t] - np.pi
+                Tp[x,t] -= 2.0*eps
+                Fp[x,t] += np.pi
+
+            if Tp[x,t] < 0.0:
+                Tp[x,t] *= -1.0
+                Fp[x,t] += np.pi
+
+            if Fp[x,t] > 2.0*np.pi:
+                Fp[x,t] -= 2.0*np.pi
+
+            if Fp[x,t] < 0.0:
+                Fp[x,t] += 2.0*np.pi
+
+    return Tp, Fp
+
+def SimpleObs(T):
+
+    Nx, Nt = T.shape
+    res1, res2 = 0.0, 0.0
+    t=0
+
+    for x in range(Nx):
+        res1 += np.sin(T[x,t])*np.sin(T[(x+1)%Nx,t])*np.sin(T[(x+2)%Nx,t])
+        res2 += np.sin(T[x,t])
+
+    return res1/Nx, res2/Nx   
+
+
+def DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow):
+
+    accept = 0 #acceptance counter
+    current_step = 0
+    act = Action(T,F,beta,coupling,s)
+
+    #first thermalize
+    for n in range(ntherm):
+
+        current_step += 1
+        Tp, Fp = Perturb(T,F,dth,dphi)
+        actp = Action(Tp,Fp,beta,coupling,s)
+        
+        if np.random.random() < min([1, np.abs( np.exp( -(actp-act) ) )]):
+            T, F, act = Tp, Fp, actp
+            accept += 1
+        else:
+            T, F, act = T, F, act
+
+    #then do the rest of the monte carlo
+    for n in range(MCsteps):
+
+
+        current_step += 1
+        Tp, Fp = Perturb(T,F,dth,dphi)
+        actp = Action(Tp,Fp,beta,coupling,s)
+        
+        if np.random.random() < min([1, np.abs( np.exp( -(actp-act) ) )]):
+            T, F, act = Tp, Fp, actp
+            accept += 1
+        else:
+            T, F, act = T, F, act
+        
+        obs1, obs2 = SimpleObs(T)
+
+        if current_step%10 == 0:
+            print("ACTION: {} {}".format(act.real,act.imag)) 
+            print("ACCEPTANCE: {}".format(accept/current_step))
+            print("OBS1: {} {}".format(obs1.real, obs1.imag))
+            print("OBS2: {} {}".format(obs2.real, obs2.imag))
+    
 
 
 
@@ -73,33 +135,35 @@ def main():
             else:
                 params.append(line)
 
-    theory, beta, coupling, s, Nt, Nx, ncfgs, ntherm, dth, dphi, Tflow = str(params[0]), float(params[1]), float(params[2]), float(params[3]), int(params[4]), int(params[5]), int(params[6]), int(params[7]), float(params[8]), float(params[9]), float(params[10])   
+    theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow = str(params[0]), float(params[1]), float(params[2]), float(params[3]), int(params[4]), int(params[5]), int(params[6]), int(params[7]), float(params[8]), float(params[9]), float(params[10])   
  
     #print inputs  
-    print("[theory, beta, coupling, s, Nt, Nx, ncfgs, ntherm, dth, dphi, Tflow] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]".format(params[0],params[1],params[2],params[3],params[4],params[5],params[6],params[7],params[8],params[9],params[10]))
+    print("[theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]".format(params[0],params[1],params[2],params[3],params[4],params[5],params[6],params[7],params[8],params[9],params[10]))
 
 
     T = np.ones((Nx, Nt), dtype = np.complex128) #represents the theta variables
     F = np.ones((Nx, Nt), dtype = np.complex128) #represents the phi   variables
 
-    for x in range(Nx):
-        for t in range(Nt):
-            T[x,t] = x + 2*t + 1
-            F[x,t] = 3*(x+1) + 5*(t+1) + 2
+    #for x in range(Nx):
+    #    for t in range(Nt):
+    #        T[x,t] = x + 2*t + 1
+    #        F[x,t] = 3*(x+1) + 5*(t+1) + 2
 
-    print(T)
-    print(F)
+    #print(T)
+    #print(F)
 
+    #Tp, Fp = Perturb(T,F,dth,dphi)
 
-    act = Action(T,F,beta,coupling,s)
-    print(act)
+    #print(Tp)
+    #print(Fp)
 
-    return 0
+    #act = Action(T,F,beta,coupling,s)
+    #print(act)
 
-    #run the MC
-    full_qmc(theory, beta, coupling, s, Nt, Nx, ncfgs, ntherm, dth, dphi, Tflow)
- 
-  
+    #return 0
+
+    DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow)
+
 
 if __name__ == "__main__":
     main()
