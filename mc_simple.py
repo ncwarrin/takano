888283a7b36dc3dc4d6c473728accfd80c9d46c3
Neill Warrington <tomu@Neills-MacBook-Pro.local>
2022-11-30 12:04:38 -0800

    Added stuff to mc_simple.py

--- mc_simple.py
+++ mc_simple.py
@@ -23,9 +23,17 @@ def S(T,F,beta,coupling,s):
             bp  *= ( np.cos(T[x,(t+1)%Nt]/2)*np.cos(T[x,t]/2)+
                      np.sin(T[x,(t+1)%Nt]/2)*np.sin(T[x,t]/2)*np.exp(1j*(F[x,(t+1)%Nt]-F[x,t])) )
 
+            tot += Ox[x,t]*Ox[(x+1)%Nx,t] + Oz[x,t]*Oz[(x+1)%Nx,t]
+            vol += np.log( ST[x,t] ) 
+            bp  += np.log( ( np.cos(T[x,(t+1)%Nt]/2)*np.cos(T[x,t]/2)+
+                     np.sin(T[x,(t+1)%Nt]/2)*np.sin(T[x,t]/2)*np.exp(1j*(F[x,(t+1)%Nt]-F[x,t])) ) )
+
     tot *= dt*(s+1)*(s+1)*coupling
 
-    return tot - np.log(vol) - 2*s*np.log(bp)
+    #return tot - np.log(vol) - 2*s*np.log(bp)
+    return tot - vol - bp
+
+
 
 #outputs the derivative of the action. this function outputs the entire vector of derivatives
 def dS(T,F,beta,coupling,s):
@@ -36,7 +44,8 @@ def dS(T,F,beta,coupling,s):
     CT, ST, CF, SF = np.cos(T), np.sin(T), np.cos(F), np.sin(F)
     CT2, ST2 = np.cos(T/2), np.sin(T/2)
 
-    grad = np.zeros(2*Nx*Nt, dtype = np.complex128) #represents the theta variables
+    gradt = np.zeros( (Nx,Nt), dtype = np.complex128) #represents the theta variables
+    gradf = np.zeros( (Nx,Nt), dtype = np.complex128) #represents the theta variables
 
 
     for x in range(Nx):
@@ -61,10 +70,10 @@ def dS(T,F,beta,coupling,s):
 
             tmp -= 1/np.tan(T[x,t]) #numpy doesn't have cotangent 
            
-            grad[idx] = tmp    
+            gradt[x,t] = tmp    
     
             #begin calculation of dS/dphi(xt)
-            idx = Nx*Nt + Nt*x + t
+            idx = Nt*x + t
             tmp = 0
  
 
@@ -82,16 +91,10 @@ def dS(T,F,beta,coupling,s):
             tmp += (2*s*1j)*num / denom
 
 
-            grad[idx] = tmp    
+            gradf[x,t] = tmp    
 
 
-    return grad
-
-
-
-
-
-
+    return gradt, gradf
 
 
 
@@ -466,6 +469,86 @@ def Hw(T,F,beta,coupling,s,W):
 
     return out
 
+def HJ(T, F, beta, coupling, s, J):
+ 
+    N = len(J[0,:])
+    out = np.zeros( (N, N), dtype = np.complex128 )
+
+    for n in range(N):
+    
+        out[:,n] = Hw(T,F,beta,coupling,s,J[:,n])
+
+    return out
+
+
+def SimpleStep(T,F,J,beta,coupling,s,Tflow,NSteps):
+    
+    if Tflow == 0:
+        return T, F, J
+
+    dt = Tflow/NSteps
+    dT, dF = dt*np.conj( dS(T,F,beta,coupling,s) )
+
+    Tout, Fout = T+dT, F+dF
+    Jout = J
+
+    return Tout, Fout, Jout
+
+
+#eventually have this flow everything; just trying the flow of points for now
+def RK4Step(T,F,J,beta,coupling,s,Tflow,NSteps):
+    
+    if Tflow == 0:
+        return T, F, J
+
+    dt = Tflow/NSteps
+
+    T1, F1 = np.conj( dS(T,F,beta,coupling,s) )
+    T2, F2 = np.conj( dS(T + dt/2*T1,F + dt/2*F1, beta, coupling,s) )
+    T3, F3 = np.conj( dS(T + dt/2*T2,F + dt/2*F2, beta, coupling,s) )
+    T4, F4 = np.conj( dS(T + dt/1*T3,F + dt/1*F3, beta, coupling,s) )
+
+    J1 = HJ(T, F, beta, coupling, s, J)
+    J2 = HJ(T+dt/2*T1, F+dt/2*F1, beta, coupling, s, J+dt/2*J1)
+    J3 = HJ(T+dt/2*T2, F+dt/2*F2, beta, coupling, s, J+dt/2*J2)
+    J4 = HJ(T+dt/1*T3, F+dt/1*F3, beta, coupling, s, J+dt/1*J3)
+
+
+    Tout, Fout, Jout = T+dt/6*( T1 + 2*T2 + 2*T3 + T4), F+dt/6*( F1 + 2*F2 + 2*F3 + F4 ), J+dt/6*( J1 + 2*J2 + 2*J3 + J4 )
+
+    return Tout, Fout, Jout
+
+
+
+
+def Flow(T, F, J, beta, coupling, s, Tflow, NSteps):
+
+
+    tic = time.perf_counter()
+ 
+    Ttmp, Ftmp, Jtmp = copy.deepcopy(T), copy.deepcopy(F), copy.deepcopy(J)
+    
+    for n in range(NSteps):
+
+        Tp, Fp, Jp = RK4Step(Ttmp,Ftmp,Jtmp,beta,coupling,s,Tflow,NSteps)
+        Ttmp, Ftmp, Jtmp = Tp, Fp, Jp
+
+    toc = time.perf_counter()
+    print("Time to flow: %0.4f seconds" %(toc-tic))   
+
+
+
+    Sinit = S(T,F,beta,coupling,s)
+    Sfin  = S(Ttmp, Ftmp, beta, coupling, s)
+    dS = Sfin - Sinit
+
+    print("dS along flow =  {} {} i".format(dS.real, dS.imag))    
+
+    return Ttmp, Ftmp, Jtmp
+
+
+
+
 
 def Hamiltonian(T,F,coupling,s):
 
@@ -496,7 +579,7 @@ def Perturb(T,F,dth,dphi):
     Tp += delta_T
     Fp += delta_F
 
-    #this step just ensures we stay in the proper integration domain
+    #this step just ensures we stay in the proper integration dom.
     for t in range(Nt):
         for x in range(Nx):
  
@@ -530,14 +613,21 @@ def SimpleObs(T):
     return res1/Nx, res2/Nx   
 
 
-def DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow):
+def DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow, NSteps):
+
 
     accept = 0 #acceptance counter
     current_step = 0
-    act = S(T,F,beta,coupling,s)
 
-    #first thermalize
-    for n in range(ntherm):
+    Id = np.identity( 2*Nx*Nt, dtype = np.complex128 )
+    J = Id
+
+    act = S(T, F, beta, coupling, s)
+
+    #first thermalize on the unflowed manifold just to find sort of typical configurations
+    print("Begin first thermalization...")
+    #for n in range(ntherm):
+    for n in range(1000):
 
         current_step += 1
         Tp, Fp = Perturb(T,F,dth,dphi)
@@ -548,32 +638,65 @@ def DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dt
             accept += 1
         else:
             T, F, act = T, F, act
+    print("...end first thermalization.")
+
+    #now that you've thermalized on the unflowed manifold, begin flowed shit
+
+    Tfar, Ffar, Jfar = Flow(T, F, Id, beta, coupling, s, Tflow, NSteps)
+
+    lndetJ = np.log(np.linalg.det(Jfar))
+    act = S(Tfar, Ffar, beta, coupling, s) - lndetJ
+
+    #second thermalize on the flowed manifold
+    print("Begin second thermalization...")
+    for n in range(ntherm):
+
+        current_step += 1
+
+        Tp, Fp = Perturb(T,F,dth,dphi)
+        Tpfar, Fpfar, Jpfar = Flow(Tp, Fp, Id, beta, coupling, s, Tflow, NSteps)
+  
+        lndetJp = np.log(np.linalg.det(Jpfar))
+        actp = S(Tpfar,Fpfar,beta,coupling,s) - lndetJp
+        
+        if np.random.random() < min([1, np.abs( np.exp( -(actp-act) ) )]):
+            T, F, Tfar, Ffar, act, lndetJ = Tp, Fp, Tpfar, Fpfar, actp, lndetJp
+            accept += 1
+        else:
+            #T, F, act = T, F, act
+            pass
+    print("...end second thermalization.")
 
     #then do the rest of the monte carlo
     for n in range(MCsteps):
 
 
         current_step += 1
+
         Tp, Fp = Perturb(T,F,dth,dphi)
-        actp = S(Tp,Fp,beta,coupling,s)
+        Tpfar, Fpfar, Jpfar = Flow(Tp, Fp, Id, beta, coupling, s, Tflow, NSteps)
+
+        lndetJp = np.log(np.linalg.det(Jpfar))
+        actp = S(Tpfar,Fpfar,beta,coupling,s) - lndetJp
         
         if np.random.random() < min([1, np.abs( np.exp( -(actp-act) ) )]):
-            T, F, act = Tp, Fp, actp
+            T, F, Tfar, Ffar, act, lndetJ = Tp, Fp, Tpfar, Fpfar, actp, lndetJp
             accept += 1
         else:
-            T, F, act = T, F, act
-        
-        obs1, obs2 = SimpleObs(T)
+            #T, F, act = T, F, act
+            pass
+  
+        #these aren't observables are were just used to debug the code
+        #obs1, obs2 = SimpleObs(T)
 
-        ham = Hamiltonian(T,F,coupling,s)
+        ham = Hamiltonian(Tfar ,Ffar , coupling, s)
 
         if current_step%10 == 0:
-            print("ACTION: {} {}".format(act.real,act.imag)) 
             print("ACCEPTANCE: {}".format(accept/current_step))
+            print("ACTION: {} {}".format(act.real,act.imag)) 
+            print("LNDETJ: {} {}".format(lndetJ.real,lndetJ.imag)) 
             print("HAMILTONIAN: {} {}".format(ham.real, ham.imag))
-            print("OBS1: {} {}".format(obs1.real, obs1.imag))
-            print("OBS2: {} {}".format(obs2.real, obs2.imag))
-    
+
 
 
 
@@ -592,16 +715,21 @@ def main():
             else:
                 params.append(line)
 
-    theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow = str(params[0]), float(params[1]), float(params[2]), float(params[3]), int(params[4]), int(params[5]), int(params[6]), int(params[7]), float(params[8]), float(params[9]), float(params[10])   
+    theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow, NSteps = str(params[0]), float(params[1]), float(params[2]), float(params[3]), int(params[4]), int(params[5]), int(params[6]), int(params[7]), float(params[8]), float(params[9]), float(params[10]), int(params[11])
  
     #print inputs  
-    print("[theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]".format(params[0],params[1],params[2],params[3],params[4],params[5],params[6],params[7],params[8],params[9],params[10]))
+    print("[theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow, NSteps] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]".format(params[0],params[1],params[2],params[3],params[4],params[5],params[6],params[7],params[8],params[9],params[10],params[11]))
+
 
     #initalize the fields
     T = np.ones((Nx, Nt), dtype = np.complex128) #represents the theta variables
     F = np.ones((Nx, Nt), dtype = np.complex128) #represents the phi   variables
 
-    DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow)
+    T += np.random.normal(scale = 0.1, size = T.shape)
+    F += np.random.normal(scale = 0.1, size = F.shape)
+
+    
+    DoTheMonteCarlo(T, F, theory, beta, coupling, s, Nt, Nx, MCsteps, ntherm, dth, dphi, Tflow, NSteps)
 
 
 if __name__ == "__main__":
@@ -610,6 +738,34 @@ if __name__ == "__main__":
 
 
 
+    #for x in range(Nx):
+    #    for t in range(Nt):
+    #        T[x,t] = Nx*x + t + 1
+
+    #F = T
+
+    #derT, derF = dS(T,F,beta,coupling,s)
+    #print("dStheta = ",derT)
+    #print("dSphi = ",derF)
+    #return 0
+
+    #testing if the flow works
+    #Tp, Fp, Jp =  Flow(T, F, J, beta, coupling, s, Tflow, NSteps)
+
+    #print("T = ",T)
+    #print("Tp = ",Tp)
+
+    #print("F = ",F)
+    #print("Fp = ",Fp)
+
+    #print("J = ",J)
+    #print("Jp = ",Jp)
+
+
+    #Sin = S(T,F,beta,coupling,s)
+    #Sout = S(Tp,Fp,beta,coupling,s)
+    #print("Sin  = ",Sin)
+    #print("Sout = ",Sout)
 
 
 
@@ -652,4 +808,39 @@ if __name__ == "__main__":
     #out = Hw(T,F,beta,coupling,s,W)
     #print(out)
 
+#def f(y):
+#
+#    return np.sin(y)*y
+#
+#def SimpleStep(y,Tflow,NSteps):
+#    
+#    dt = Tflow/NSteps
+#    dy = dt*f(y)
+#
+#    return y + dy
+#
+#
+#def RK4Step(y,Tflow,NSteps):
+#    
+#    dt = Tflow/NSteps
+#
+#    y1 = f(y)
+#    y2 = f(y + dt/2*y1)
+#    y3 = f(y + dt/2*y2)
+#    y4 = f(y + dt/1*y3)
+#
+#    return y + dt/6*( y1 + 2*y2 + 2*y3 + y4 )
+#
+#
+#def Flow(y0,Tflow,NSteps):
+# 
+#    ytmp, ytmp4 = copy.deepcopy(y0), copy.deepcopy(y0)
+#
+#    for n in range(NSteps):
+#
+#        yp  = SimpleStep(ytmp, Tflow,NSteps)
+#        yp4 = RK4Step(ytmp4, Tflow, NSteps)  
+#        ytmp, ytmp4 = yp, yp4
+#
+#    return yp, yp4
 
